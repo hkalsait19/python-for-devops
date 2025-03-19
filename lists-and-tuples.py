5436202d2b43990d381b88f990c1a5d93a24474f
hkalsait19 <harshavardhan.kalsait19@gmail.com>
2025-03-19 12:12:46 +0530

    added 07 lists-and-tuples

---
+++ lists-and-tuples.py
@@ -0,0 +1,82 @@
+##### ----- LIST & TUPLE ----- #####
+
+# students_name = ["Abhishek", "John", "Tim", "Ram", "Anything"]
+# print(students_name)
+
+
+## type of the list: <class 'list'> - using [] - square brackets
+# s3_bucket_list = ["dev_bucket", "prod_bucket", "test_bucket"]
+# print(type(s3_bucket_list))
+
+
+## type of the tuples: <class 'tuple'> - using () - round brackets
+# s3_bucket_list = ("dev_bucket", "prod_bucket", "test_bucket")
+# print(type(s3_bucket_list))
+
+## adding the elements in the list
+# s3_bucket_list = ["dev_bucket", "prod_bucket", "test_bucket"]
+# s3_bucket_list.append("new_bucket")
+# print(s3_bucket_list)
+
+## removing the elements in the list
+# s3_bucket_list = ["dev_bucket", "prod_bucket", "test_bucket"]
+# s3_bucket_list.remove("dev_bucket")
+# s3_bucket_list.remove("prod_bucket")
+# print(s3_bucket_list)
+
+## adding the elements in the tuples
+# s3_bucket_list = ("dev_bucket", "prod_bucket", "test_bucket")
+# s3_bucket_list.append("new_bucket")
+# print(s3_bucket_list)
+
+## removing the elements in the tuples
+# s3_bucket_list = ("dev_bucket", "prod_bucket", "test_bucket")
+# s3_bucket_list.remove("dev_bucket")
+# s3_bucket_list.remove("prod_bucket")
+# print(s3_bucket_list)
+
+## position of list startes form 0th location as a 1st element
+# s3_bucket_list = ["dev_bucket", "prod_bucket", "test_bucket"]
+# print(s3_bucket_list(0))
+
+
+## length (len) of the list = 3
+# s3_bucket_list = ["dev_bucket", "prod_bucket", "test_bucket"]
+# print(len(s3_bucket_list))
+
+# s3_bucket_list.append("new_bucket")
+# print(len(s3_bucket_list))
+
+# s3_bucket_list.remove("dev_bucket")
+# s3_bucket_list.remove("prod_bucket")
+# print(len(s3_bucket_list))
+
+
+
+## length (len) of the tuple = 3
+# s3_bucket_list = ("dev_bucket", "prod_bucket", "test_bucket")
+# print(len(s3_bucket_list))
+
+
+## sort the order of random numbers:
+# numbers = [1, 2, 6,8,9,3,60,0,211,4]
+# numbers.sort()
+# print(numbers)
+
+## concatination or adding list elements
+# s3_bucket_list = ["dev_bucket", "prod_bucket", "test_bucket"]
+# print(s3_bucket_list[0] + "--" + s3_bucket_list[1] + "--" + s3_bucket_list[2])
+
+## LIST is hetorgenous data type - store any data types inside in it [str, int, float, bool, list, tuple, dict, set, etc]
+# random_numbers = [0,1,2, "anynumber" , 7.7, 8.8, True, False, [1,2,3], (4,5,6), {"name": "Abhishek"}, {7,8,9}]
+# print(random_numbers)
+# print(random_numbers[2])
+# print(random_numbers[3])
+# print(random_numbers[4])
+# print(random_numbers[5])
+# print(random_numbers[6])
+# print(random_numbers[7])
+# print(random_numbers[8])
+# print(random_numbers[9])
+# print(random_numbers[10])
+# print(random_numbers[11])
