ee4340fd2a5adaf1b7f735347b88756031462056
hkalsait19 <harshavardhan.kalsait19@gmail.com>
2025-03-19 14:48:56 +0530

    added 08 loops

---
+++ 08/loops.py
@@ -0,0 +1,13 @@
+## FOR loop definite times execute - we know the sequence
+
+
+# for i in range(10):
+#     print(i)
+
+
+## WHILE loop indefinite times execute - we don't know the sequence
+
+
+colors=["red", "green", "blue", "yellow", "black", "white", "pink", "purple", "orange", "brown"]
+for color in colors:
+    print(color)
