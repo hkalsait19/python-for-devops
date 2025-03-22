d65e69d0edd34cca86c1bb7e0687095cd5d8d9a6
hkalsait19 <harshavardhan.kalsait19@gmail.com>
2025-03-22 19:44:29 +0530

    added 10-user-input 11-req 00-basic dir

---
+++ 00/00.py
@@ -0,0 +1,18 @@
+
+
+# import sys
+# print(sys.version)
+
+
+## unpacking of variables
+# fruits=["apple","banana","cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+a = 33
+b = 100
+
+if b > a:
+  print("b is greater than a")
