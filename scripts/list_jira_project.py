da229981ed220a1dfb1e0b6d13fdcd19ddea1308
hkalsait19 <harshavardhan.kalsait19@gmail.com>
2025-04-07 21:16:04 +0530

    added create issue n list jira script

---
+++ scripts/list_jira_project.py
@@ -0,0 +1,24 @@
+# This code sample uses the 'requests' library:
+# http://docs.python-requests.org
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+url = "https://yourblahblah.atlassian.net/rest/api/3/project"
+
+API_TOKEN = "your API token"
+
+auth = HTTPBasicAuth("your email ID", "API_TOKEN")
+
+headers = {
+  "Accept": "application/json"
+}
+
+response = requests.request(
+   "GET",
+   url,
+   headers=headers,
+   auth=auth
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
